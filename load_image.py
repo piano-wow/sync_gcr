7294333b26793ce50398fac27291925dee0a3036
piano <35907674+piano-wow@users.noreply.github.com>
2023-01-06 17:01:20 +0800

    Update load_image.py

--- load_image.py
+++ load_image.py
@@ -1,19 +1,22 @@
 # coding:utf-8
+# 支持 python2
 import subprocess, os
 def get_filename():
     with open("images.txt", "r") as f:
         lines = f.read().split('\n')
-        # print(lines)
         return lines
-
 
 
 def pull_image():
     name_list= get_filename()
     for name in name_list:
-        new_name = "kenwood/" + name.split("/")[-1]
-        subprocess.call("docker pull {}".format(new_name), shell=True)
-        subprocess.run(["docker", "tag", new_name, name])
-        subprocess.call("docker rmi  {}".format(new_name), shell= True)
+        if  name:
+            new_name = "kenwood/" + name.split("/")[-1]
+            cmd = "echo " + "docker pull " + new_name
+            subprocess.call(cmd, shell=True)
+            cmd = "echo " + "docker tag " + new_name + " " + name
+            subprocess.call(cmd, shell=True)
+            cmd = "echo " + "docker rmi " + new_name
+            subprocess.call(cmd, shell= True)
 if __name__ == "__main__":
     pull_image()
